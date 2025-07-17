83fa2f2b05c9953ae339c0df965ad16bf3bbd938
jeet <pateljeet355@gmail.com>
2025-07-17 10:56:06 +0530

    Added Docker, Terraform, and GitLab CI/CD files for deployment

---
+++ terraform/ec2.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+resource "aws_instance" "mern_app" {
+  ami           = "ami-06f621d90fa29f6d0" # Amazon Linux 2023 (Mumbai)
+  instance_type = "t2.micro"
+  key_name      = "mywebserver-key"
+
+  tags = {
+    Name = "MERNApp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker pull $CI_REGISTRY_IMAGE/frontend:$CI_COMMIT_SHORT_SHA",
+      "sudo docker pull $CI_REGISTRY_IMAGE/backend:$CI_COMMIT_SHORT_SHA",
+      "sudo docker run -d -p 3000:3000 $CI_REGISTRY_IMAGE/frontend:$CI_COMMIT_SHORT_SHA",
+      "sudo docker run -d -p 5000:5000 $CI_REGISTRY_IMAGE/backend:$CI_COMMIT_SHORT_SHA"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = var.ec2_host
+    }
+  }
+} 
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "ap-south-1"
+}
+variable "ec2_host" {
+  default = "13.204.65.121"
+} 
